49182615c7fa91a005314bba596e9584e301dc7f
Waqas Bajwa <waqasbaj@gmail.com>
2018-01-18 22:12:27 +0000

    Call back function returned

---
+++ using_closure_stretch.js
@@ -0,0 +1,44 @@
+var wrapLog = function (callback, name)
+
+{
+
+  return function(x,y,z)
+  {
+    console.log(name +"("+ x +", " +y +") ==> ", callback(x,y,z));
+
+  };
+
+};
+
+var area = function (x, y) {
+  return x * y;
+};
+var logArea = wrapLog(area, "area");
+
+logArea(5, 3); // area(5, 3) => 15
+logArea(3, 2); // area(3, 2) => 6
+
+var volume = function (x, y, z) {
+  return x * y * z;
+};
+var logVolume = wrapLog(volume, "volume");
+
+logVolume(5, 3, 2); // volume(5, 3, 2) => 30
+logVolume(3, 2, 4); // volume(3, 2, 4) => 24
+
+
+
+
+
+
+// function makeAdder(x) {
+//   return function(y) {
+//     return y+y;
+//   };
+// }
+// var z = makeAdder(3)(4);
+
+// console.log(z);
+
+
+
